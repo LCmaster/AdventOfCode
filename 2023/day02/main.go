a085e1b945d46fbb53a3e37ea46887aa66b6d354
Luiz Vicente <luizcv89@gmail.com>
2023-12-02 14:43:58 -0300

    Solve Day 02 Part 2

--- 2023/day02/main.go
+++ 2023/day02/main.go
@@ -53,6 +53,40 @@ func part1(input []string, maxRedAmount int, maxGreenAmount int, maxBlueAmount i
     return output
 }
 
+func part2(input []string) int {
+    gameMatcher := regexp.MustCompile("^Game ([0-9]+): (.*;*)")
+    groupMatcher := regexp.MustCompile("([0-9]+) (red|green|blue)")
+
+    output := 0
+
+    for _, line := range input {
+        gameMatch := gameMatcher.FindStringSubmatch(line)
+        plays := gameMatch[2]
+
+        totalCubes := map[string]int {"red": 0, "green": 0, "blue": 0}
+
+        for _, play := range strings.Split(plays, ";") {
+            for _, group := range strings.Split(play, ",") {
+                groupMatch := groupMatcher.FindStringSubmatch(group)
+                number, err := strconv.Atoi(groupMatch[1])
+                if err != nil {
+                    break
+                }
+                color := groupMatch[2]
+
+                if number > totalCubes[color] {
+                    totalCubes[color] = number
+                }
+            }
+        }
+        output += totalCubes["red"] * totalCubes["green"] * totalCubes["blue"]
+    }
+
+    return output
+}
+
+
+
 func main() {
     file, err := os.Open("./2023/day02/input.txt")
     if err != nil {
@@ -77,4 +111,6 @@ func main() {
 
     answer1 := part1(buffer, possibleRedCubesAmount, possibleGreenCubesAmount, possibleBlueCubesAmount)
     fmt.Println("Answer to part 1:", answer1)
+    answer2 := part2(buffer, possibleRedCubesAmount, possibleGreenCubesAmount, possibleBlueCubesAmount)
+    fmt.Println("Answer to part 2:", answer2)
 }
--- 2023/day02/main_test.go
+++ 2023/day02/main_test.go
@@ -21,3 +21,20 @@ func TestPart1(t *testing.T) {
     }
 }
 
+func TestPart2(t *testing.T) {
+    input := []string{
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    }
+    
+    output := part2(input, 12, 13, 14) 
+    expected := 2286
+    
+    if output != expected {
+        t.Errorf("TestPart2: Output: %d, Expected: %d", output, expected)
+    }
+}
+
