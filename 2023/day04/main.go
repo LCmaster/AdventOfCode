0f238fb5328953024de7b6b6f7fe4a3db456ec51
Luiz Vicente <luizcv89@gmail.com>
2023-12-04 13:24:55 -0300

    Solve Day 04 Part 2

--- 2023/day04/main.go
+++ 2023/day04/main.go
@@ -46,6 +46,52 @@ func part1(input []string) int {
     return output
 }
 
+func part2(input []string) int {
+    output:= 0
+
+    instances := make(map[int]int)
+    for id, line := range input {
+        gameId := id+1
+        instances[gameId] += 1
+
+        parts := strings.Split(line, ":")
+        if len(parts) == 2 {
+            // card := parts[0]
+            game := parts[1];
+            numbers := strings.Split(game, "|")
+            numbersToCheck := strings.Split(numbers[0], " ")
+            winningNumbers := strings.Split(numbers[1], " ")
+
+            cardPoints := 0
+            for _, numberToCheckStr := range numbersToCheck {
+                for _, winningNumberStr := range winningNumbers {
+                    numberToCheck, err1 := strconv.Atoi(numberToCheckStr)
+                    winningNumber, err2 := strconv.Atoi(winningNumberStr)
+
+                    if err1 == nil && err2 == nil {
+                        if numberToCheck == winningNumber {
+                            cardPoints += 1
+                        }
+                    }
+                }
+            }
+
+            for i := 0; i < instances[gameId]; i++ {
+                for j:= 0; j < cardPoints; j++ {
+                    instances[gameId+j+1] += 1
+                }
+            }
+        }
+    }
+
+    for _, count := range instances {
+        output += count
+    }
+
+    return output
+}
+
+
 func main() {
     input, err := utils.ReadInputFile(2023, 4)
     if err != nil {
@@ -53,5 +99,7 @@ func main() {
     }
 
     answer1 := part1(input)
+    answer2 := part2(input)
     fmt.Println("Answer to part 1:", answer1)
+    fmt.Println("Answer to part 2:", answer2)
 }
