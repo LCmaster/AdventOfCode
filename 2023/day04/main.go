d7a522bb6eca9f6240fa172f624aeb5dd0722a2b
Luiz Vicente <luizcv89@gmail.com>
2023-12-04 12:16:22 -0300

    Solve Day 04 Part 1

---
+++ 2023/day04/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/LCmaster/AdventOfCode/utils"
+)
+
+func part1(input []string) int {
+    output:= 0
+
+    for _, line := range input {
+        parts := strings.Split(line, ":")
+        if len(parts) == 2 {
+            // card := parts[0]
+            game := parts[1];
+            numbers := strings.Split(game, "|")
+            numbersToCheck := strings.Split(numbers[0], " ")
+            winningNumbers := strings.Split(numbers[1], " ")
+
+            cardPoints := 0
+            for _, numberToCheckStr := range numbersToCheck {
+                for _, winningNumberStr := range winningNumbers {
+                    numberToCheck, err1 := strconv.Atoi(numberToCheckStr)
+                    winningNumber, err2 := strconv.Atoi(winningNumberStr)
+
+                    if err1 == nil && err2 == nil {
+                        if numberToCheck == winningNumber {
+                            if cardPoints == 0 {
+                                cardPoints += 1
+                            } else {
+                                cardPoints *= 2
+                            }
+                        }
+                    }
+                }
+            }
+
+            output += cardPoints
+        }
+    }
+
+    return output
+}
+
+func main() {
+    input, err := utils.ReadInputFile(2023, 4)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    answer1 := part1(input)
+    fmt.Println("Answer to part 1:", answer1)
+}
