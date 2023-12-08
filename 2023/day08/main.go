e2e1cbfcf259c4b4ab1bc979982a0fd9f7d3eb07
Luiz Vicente <luizcv89@gmail.com>
2023-12-08 16:42:46 -0300

    Solve Day 08 Part 2

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func extractNodes(input []string) map[string][]string{
-    nodeMatcher := regexp.MustCompile("^([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)$")
+    nodeMatcher := regexp.MustCompile("^([a-zA-Z0-9]{3}) = \\(([a-zA-Z0-9]{3}), ([a-zA-Z0-9]{3})\\)$")
     nodes := make(map[string][]string)
     for _, line := range input {
         match := nodeMatcher.FindStringSubmatch(strings.TrimSpace(line))
@@ -22,16 +22,31 @@ func extractNodes(input []string) map[string][]string{
     return nodes
 }
 
+func gcd(a, b int) int {
+    for b != 0 {
+        t := b
+        b = a % b
+        a = t
+    }
+
+    return a
+}
+
+func lcm(a, b int) int {
+    return a * b / gcd(a, b)
+}
+
+
 func part1(input []string) int {
     output := 0
     instructions := input[0]
     nodes := extractNodes(input)
 
     currentNode := "AAA"
-    for currentNode != "ZZZ" {
+    for true {
         for _, direction := range instructions {
             if currentNode == "ZZZ" {
-                break
+                return output
             }
 
             if direction == 'L' {
@@ -43,8 +58,52 @@ func part1(input []string) int {
         }
     }
 
+    return 0
+}
+
+func part2(input []string) int {
+    output := 0
+    instructions := input[0]
+    nodes := extractNodes(input)
+
+    currentNodes := []string{}
+    for node := range nodes {
+        if node[2] == 'A' {
+            currentNodes = append(currentNodes, node)
+        }
+    }
+
+    if len(currentNodes) > 0 {
+        stepsMultiplier := []int{}
+        for _, node := range currentNodes {
+            steps := 0
+            Route:
+            for true {
+                for _, direction := range instructions {
+                    if node[2] == 'Z' {
+                        break Route
+                    }
+                    if direction == 'L' {
+                        node = nodes[node][0]
+                    } else {
+                        node = nodes[node][1]
+                    }
+
+                    steps++
+                }
+            }
+            stepsMultiplier = append(stepsMultiplier, steps)
+        }
+        result := stepsMultiplier[0]
+        for i := 1; i < len(stepsMultiplier); i++ {
+            result = lcm(result, stepsMultiplier[i])
+        }
+        output = result
+    }
+
     return output
 }
+
 
 func main() {
     input, err := utils.ReadInputFile(2023, 8)
@@ -53,4 +112,5 @@ func main() {
     }
 
     fmt.Println("Answer to part 1:", part1(input))
+    fmt.Println("Answer to part 2:", part2(input))
 }
--- 2023/day08/main_test.go
+++ 2023/day08/main_test.go
@@ -19,3 +19,24 @@ func TestPart1(t *testing.T) {
     }
 }
 
+func TestPart2(t *testing.T) {
+    input := []string{
+        "LR",
+        "11A = (11B, XXX)",
+        "11B = (XXX, 11Z)",
+        "11Z = (11B, XXX)",
+        "22A = (22B, XXX)",
+        "22B = (22C, 22C)",
+        "22C = (22Z, 22Z)",
+        "22Z = (22B, 22B)",
+        "XXX = (XXX, XXX)",
+    }
+
+    output := part2(input)
+    expected := 6
+
+    if output != expected {
+        t.Errorf("Day08 TestPart2 => Output: %d, Expected: %d", output, expected)
+    }
+}
+
