e82e7ffba71e0874ec06a174889a90ccaea0686d
Luiz Vicente <luizcv89@gmail.com>
2023-12-14 16:31:55 -0300

    Solve Day 14 Part 2

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -8,8 +8,7 @@ import (
 	"github.com/LCmaster/AdventOfCode/utils"
 )
 
-func part1(input []string) int {
-    output := 0
+func slideNorth(input []string) []string {
     for i := 0; i < len(input); i++ {
         for j:= 0; j < len(input[i]); j++ {
             if i > 0 && input[i][j] == 'O' && input[i-1][j] == '.'{
@@ -26,11 +25,109 @@ func part1(input []string) int {
             }
         }
     }
-    for i, line := range input {
-        output += strings.Count(line, "O") * (len(input) - i)
-    }
-    return output
+    return input
 }
+
+func slideWest(input []string) []string {
+    for i := 0; i < len(input); i++ {
+        for j := 0; j < len(input[i]); j++ {
+            if j > 0 && input[i][j] == 'O' && input[i][j-1] == '.'{
+                k := 1
+                for (j-(k+1)) >= 0 && input[i][j-(k+1)] == '.' {
+                    k++
+                }
+                line := []rune(input[i])
+                line[j] = '.'
+                line[j-k] = 'O'
+                input[i] = string(line)
+            }
+        }
+    }
+    return input
+}
+
+func slideSouth(input []string) []string {
+    for i := len(input)-1; i >= 0; i-- {
+        for j:= 0; j < len(input[i]); j++ {
+            if i < len(input)-1 && input[i][j] == 'O' && input[i+1][j] == '.'{
+                k := 1
+                for (i+k+1) <= len(input)-1 && input[i+k+1][j] == '.' {
+                    k++
+                }
+                north := []rune(input[i])
+                south := []rune(input[i+k])
+                north[j] = '.'
+                south[j] = 'O'
+                input[i] = string(north)
+                input[i+k] = string(south)
+            }
+        }
+    }
+    return input
+}
+
+func slideEast(input []string) []string {
+    for i := 0; i < len(input); i++ {
+        for j := len(input[i]) - 1; j >= 0; j-- {
+            if j < len(input[i])-1 && input[i][j] == 'O' && input[i][j+1] == '.'{
+                k := 1
+                for (j+k+1) <= len(input[i])-1 && input[i][j+k+1] == '.' {
+                    k++
+                }
+                line := []rune(input[i])
+                line[j] = '.'
+                line[j+k] = 'O'
+                input[i] = string(line)
+            }
+        }
+    }
+    return input
+}
+
+func runCycle(input []string) []string {
+    return slideEast(slideSouth(slideWest(slideNorth(input))))
+}
+
+func calculateLoad(input []string) int {
+    result := 0
+    for i, line := range input {
+        result += strings.Count(line, "O") * (len(input) - i)
+    }
+    return result
+}
+
+func generateKey(input []string) string {
+    return strings.Join(input, ",")
+}
+
+func part1(input []string) int {
+    return calculateLoad(slideNorth(input))
+}
+
+func part2(input []string, cycles int) int {
+    cache := make(map[string]string)
+    key := generateKey(input)
+    i := 0
+    for i < cycles {
+        if next, ok := cache[key]; ok {
+            if i + len(cache) < cycles {
+                i += len(cache)
+            }else {
+                i++
+            }
+            key = next
+        } else {
+            result := runCycle(strings.Split(key, ","))
+            next := generateKey(result)
+            cache[key] = next
+            
+            key = next
+            i++
+        }
+    }
+    return calculateLoad(strings.Split(key, ","))
+}
+
 
 func main() {
     input, err := utils.ReadInputFile(2023, 14)
@@ -39,4 +136,5 @@ func main() {
     }
 
     fmt.Println("Answer to part 1:", part1(input))
+    fmt.Println("Answer to part 2:", part2(input, 1000000000))
 }
--- 2023/day14/main_test.go
+++ 2023/day14/main_test.go
@@ -22,3 +22,24 @@ func TestPart1(t *testing.T) {
         t.Errorf("Day14 TestPart1 => Output: %d, Expected: %d", output, expected)
     }
 }
+
+func TestPart2(t *testing.T) {
+    input := []string {
+        "O....#....",
+        "O.OO#....#",
+        ".....##...",
+        "OO.#O....O",
+        ".O.....O#.",
+        "O.#..O.#.#",
+        "..O..#O..O",
+        ".......O..",
+        "#....###..",
+        "#OO..#....",
+    }
+    output := part2(input, 1000000000)
+    expected := 64
+
+    if output != expected {
+        t.Errorf("Day14 TestPart2 => Output: %d, Expected: %d", output, expected)
+    }
+}
