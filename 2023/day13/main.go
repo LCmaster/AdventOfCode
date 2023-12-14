ded98d1b3c59bcbc069eb3418fe9fe641afda706
Luiz Vicente <luizcv89@gmail.com>
2023-12-13 23:39:29 -0300

    Solve Day 13 Part 2

--- 2023/day13/main.go
+++ 2023/day13/main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/LCmaster/AdventOfCode/utils"
 )
 
+func getDifferenceCount(line1, line2 string) int {
+    diff := 0
+    for i := 0; i < len(line1); i++ {
+        if line1[i] != line2[i] {
+            diff++
+        }
+    }
+    return diff
+}
+
 func getColumn(buffer []string, col int) string {
     column := ""
     for j := 0; j < len(buffer); j++ {
@@ -15,21 +25,24 @@ func getColumn(buffer []string, col int) string {
     return column
 }
 
-func checkMirroredColumns(buffer []string) int {
+func checkMirroredColumns(buffer []string, smudge int) int {
     prevColumn := ""
     for i := 0; i < len(buffer[0]); i++ {
         column := getColumn(buffer, i)
-        if column == prevColumn {
-            symetric := true
-            j := i-1
-            k := i
-            for j >= 0 && k < len(buffer[0]) {
-                symetric = symetric && getColumn(buffer, j) == getColumn(buffer, k)
-                j--
-                k++
-            }
-            if symetric {
-                return i
+
+        if prevColumn != "" {
+            diff := getDifferenceCount(prevColumn, column)
+            if diff <= smudge {
+                j := i-2
+                k := i+1
+                for j >= 0 && k < len(buffer[0]) {
+                    diff += getDifferenceCount(getColumn(buffer, j), getColumn(buffer, k))
+                    j--
+                    k++
+                }
+                if diff == smudge {
+                    return i
+                }
             }
         }
         prevColumn = column
@@ -38,23 +51,27 @@ func checkMirroredColumns(buffer []string) int {
     return 0
 }
 
-func checkMirroredRows(buffer []string) int {
+func checkMirroredRows(buffer []string, smudge int) int {
     prevLine := ""
     for i := 0; i < len(buffer); i++ {
-        if buffer[i] == prevLine {
-            symetric := true
-            j := i-1
-            k := i
-            for j >= 0 && k < len(buffer) {
-                symetric = symetric && buffer[j] == buffer[k]
-                j--
-                k++
-            }
-            if symetric {
-                return i
+        line := buffer[i]
+
+        if  prevLine != ""{
+            diff := getDifferenceCount(prevLine, line)
+            if diff <= smudge {
+                j := i-2
+                k := i+1
+                for j >= 0 && k < len(buffer) {
+                    diff += getDifferenceCount(buffer[j], buffer[k])
+                    j--
+                    k++
+                }
+                if diff == smudge {
+                    return i
+                }
             }
         }
-        prevLine = buffer[i]
+        prevLine = line
     }
 
     return 0
@@ -63,26 +80,39 @@ func checkMirroredRows(buffer []string) int {
 func part1(input []string) int {
     output := 0
     buffer := []string{}
-    result := []int{}
 
-    count := 0
     for _, line := range input {
         if len(line) > 0 {
             buffer = append(buffer, line)
             continue
         } else {
-            result = append(result, checkMirroredColumns(buffer))
-            result = append(result, checkMirroredRows(buffer))
+            output += checkMirroredColumns(buffer, 0)
+            output += 100 * checkMirroredRows(buffer, 0)
             buffer = []string{}
-            count++
         }
     }
 
-    for i := 0; i < len(result); i += 2 {
-        output += result[i] + (100 * result[i+1])
-    }
     return output
 }
+
+func part2(input []string) int {
+    output := 0
+    buffer := []string{}
+
+    for _, line := range input {
+        if len(line) > 0 {
+            buffer = append(buffer, line)
+            continue
+        } else {
+            output += checkMirroredColumns(buffer, 1)
+            output += 100*checkMirroredRows(buffer, 1)
+            buffer = []string{}
+        }
+    }
+
+    return output
+}
+
 
 func main() {
     input, err := utils.ReadInputFile(2023, 13)
@@ -90,4 +120,5 @@ func main() {
         log.Fatal(err)
     }
     fmt.Println("Answer to part 1:", part1(input))
+    fmt.Println("Answer to part 2:", part2(input))
 }
--- 2023/day13/main_test.go
+++ 2023/day13/main_test.go
@@ -28,3 +28,30 @@ func TestPart1(t *testing.T) {
 	t.Errorf("Day13 TestPart1 => Output: %d, Expected: %d", output, expected)
     }
 }
+
+func TestPart2(t *testing.T) {
+    input := []string{
+	"#.##..##.",
+	"..#.##.#.",
+	"##......#",
+	"##......#",
+	"..#.##.#.",
+	"..##..##.",
+	"#.#.##.#.",
+	"",
+	"#...##..#",
+	"#....#..#",
+	"..##..###",
+	"#####.##.",
+	"#####.##.",
+	"..##..###",
+	"#....#..#",
+	"",
+    }
+    output := part2(input)
+    expected := 400
+
+    if output != expected {
+	t.Errorf("Day13 TestPart2 => Output: %d, Expected: %d", output, expected)
+    }
+}
